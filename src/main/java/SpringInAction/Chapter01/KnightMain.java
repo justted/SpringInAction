995ec76ce906cc7e519520978563fe21d0124092
Justted <1096661427@qq.com>
2017-09-11 18:35:19 +0800

    第一章

--- src/main/java/SpringInAction/Chapter01/KnightMain.java
+++ src/main/java/SpringInAction/Chapter01/KnightMain.java
@@ -8,7 +8,7 @@ public class KnightMain {
   public static void main(String[] args) throws Exception {
     ClassPathXmlApplicationContext context = 
         new ClassPathXmlApplicationContext(
-            "META-INF/spring/knight.xml","META-INF/spring/minstrel.xml");
+                "SpringInAction/Chapter01/knight.xml", "SpringInAction/Chapter01/minstrel.xml");
     Knight knight = context.getBean(Knight.class);
     knight.embarkOnQuest();
     context.close();
