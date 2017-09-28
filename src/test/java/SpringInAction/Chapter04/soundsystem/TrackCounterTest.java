ab5dd33b10fa6bbf8b19ef804dba8c842ffa4fe5
Justted <1096661427@qq.com>
2017-09-28 20:34:41 +0800

    第四章面向切面

--- src/test/java/SpringInAction/Chapter04/soundsystem/TrackCounterTest.java
+++ src/test/java/SpringInAction/Chapter04/soundsystem/TrackCounterTest.java
@@ -9,7 +9,7 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import static org.junit.Assert.*;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(classes = TrackCounterConfig.class)
+@ContextConfiguration(locations = {"classpath:SpringInAction/Chapter04/soundsystem/TrackCounter.xml"})
 public class TrackCounterTest {
     @Autowired
     private CompactDisc cd;
