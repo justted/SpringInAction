1738fd316c576f961e83392dcff14fe76856a629
Justted <1096661427@qq.com>
2017-10-09 18:46:14 +0800

    第五章SpringMVC

--- Chapter05/src/main/java/spittr/web/WebConfig.java
+++ Chapter05/src/main/java/spittr/web/WebConfig.java
@@ -20,6 +20,7 @@ public class WebConfig extends WebMvcConfigurerAdapter {
     InternalResourceViewResolver resolver = new InternalResourceViewResolver();
     resolver.setPrefix("/WEB-INF/views/");
     resolver.setSuffix(".jsp");
+    resolver.setExposeContextBeansAsAttributes(true);
     return resolver;
   }
   
