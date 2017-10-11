787744e1d95325c2fb641af3a930661998830e01
Justted <1096661427@qq.com>
2017-10-11 10:22:33 +0800

    第五章SpringMVC

---
+++ Chapter05/src/main/java/spittr/web/SpittleController.java
@@ -0,0 +1,58 @@
+package spittr.web;
+
+import java.util.Date;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import spittr.Spittle;
+import spittr.data.SpittleRepository;
+
+@Controller
+@RequestMapping("/spittles")
+public class SpittleController {
+
+  private static final String MAX_LONG_AS_STRING = "9223372036854775807";
+  
+  private SpittleRepository spittleRepository;
+
+  @Autowired
+  public SpittleController(SpittleRepository spittleRepository) {
+    this.spittleRepository = spittleRepository;
+    //这里注入的当然就是spittleRepository的具体实现类JdbcSpittleRepository
+  }
+
+  @RequestMapping(method=RequestMethod.GET)
+  public List<Spittle> spittles(
+      @RequestParam(value="max", defaultValue=MAX_LONG_AS_STRING) long max,
+      @RequestParam(value="count", defaultValue="20") int count) {
+    return spittleRepository.findSpittles(max, count);
+  }
+  //还有三种方法见书本，其余三种方法都显式的表现了视图的名称。这里因为此方法针对“/spittles”的请求，所以认为视图的名称将会是spittles
+
+
+  @RequestMapping(value="/{spittleId}", method=RequestMethod.GET)
+  public String spittle(
+      @PathVariable("spittleId") long spittleId, 
+      Model model) {
+    model.addAttribute(spittleRepository.findOne(spittleId));
+    return "spittle";
+  }
+
+  @RequestMapping(method=RequestMethod.POST)
+  //处理用户提交的表单数据（spittles，吐槽），仔重定向至/spittles
+  //这里涉及到了controller接收表单数据的问题，这里是通过一个bean来接收的，比较方便
+  //id应该是自增的
+  public String saveSpittle(SpittleForm form, Model model) throws Exception {
+    spittleRepository.save(new Spittle(null, form.getMessage(), new Date(), 
+        form.getLongitude(), form.getLatitude()));
+    return "redirect:/spittles";
+  }
+
+}
