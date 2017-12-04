ddf03c8c8bc7035a7d8cd070d6c39ecd9b559f44
Justted <1096661427@qq.com>
2017-12-04 20:19:26 +0800

    第九章SpringSecurity

--- Chapter09/src/main/java/spittr/config/SecurityConfig.java
+++ Chapter09/src/main/java/spittr/config/SecurityConfig.java
@@ -40,7 +40,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
   protected void configure(AuthenticationManagerBuilder auth) throws Exception {
     auth
       .inMemoryAuthentication()
-        .withUser("user").password("password").roles("USER");
+        .withUser("user").password("password").roles("USER")
+      .and()
+        .withUser("admin").password("password").roles("USER", "ADMIN");
   }
 
   
