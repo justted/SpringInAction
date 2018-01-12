d997b18c1c4e4b1e2935b6920ec88800cfbb9f4c
Justted <1096661427@qq.com>
2018-01-12 21:24:28 +0800

    第十一章使用对象-关系映射持久化数据

--- Chapter11/jpa-springdata/src/main/java/spittr/db/SpitterRepository.java
+++ Chapter11/jpa-springdata/src/main/java/spittr/db/SpitterRepository.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import org.springframework.data.jpa.repository.Query;
 import spittr.domain.Spitter;
 
 /**
@@ -15,5 +16,8 @@ public interface SpitterRepository extends JpaRepository<Spitter, Long>, Spitter
 	Spitter findByUsername(String username);
 	
 	List<Spitter> findByUsernameOrFullNameLike(String username, String fullName);
+
+	@Query("select s from Spitter s where s.email like '%gmail.com'")
+	List<Spitter> findAllGmailSpitters();
 
 }
